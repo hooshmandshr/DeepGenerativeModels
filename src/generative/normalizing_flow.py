a1050b8f35eeff9085d2e817edb51b11243d5b08
hooshmandshr <shokri.hooshmand@gmail.com>
2018-04-02 14:37:17 -0400

    Added dynamical flow implementation.

--- src/generative/normalizing_flow.py
+++ src/generative/normalizing_flow.py
@@ -10,19 +10,19 @@ class PlanarFlow(object):
             self.b = b
             self.u = u
         # Enforcing reversibility.
-        self.u_bar = self.reversible_constraint(self.u, self.w)
+        self.u_bar = self.reversible_constraint()
 
     def create_flow_variables(self):
         self.w = tf.Variable(np.random.normal(0, 1, [self.dim, 1]))
         self.b = tf.Variable(np.random.normal(0, 1, 1))
         self.u = tf.Variable(np.random.normal(0, 1, [1, self.dim]))
 
-    def reversible_constraint(self, u, w):
-        dot = tf.squeeze(tf.matmul(u, w))
+    def reversible_constraint(self):
+        dot = tf.squeeze(tf.matmul(self.u, self.w))
         scalar = - 1 + tf.nn.softplus(dot) - dot
-        norm_squared = tf.reduce_sum(w * w)
-        comp = scalar * tf.transpose(w) / norm_squared
-        return u + comp
+        norm_squared = tf.reduce_sum(self.w * self.w)
+        comp = scalar * tf.transpose(self.w) / norm_squared
+        return self.u + comp
 
     def transform(self, inputs):
         dialation = tf.matmul(inputs, self.w) + self.b
@@ -33,6 +33,7 @@ class PlanarFlow(object):
         psi = 1.0 - tf.pow(tf.tanh(dialation), 2)
         det_jac = tf.matmul(self.u_bar, self.w) * psi
         return - tf.squeeze(tf.log(tf.abs(1 + det_jac)))
+
 
 class FlowRandomVariable(object):
 
@@ -61,3 +62,54 @@ class FlowRandomVariable(object):
         for flow in self.flows:
             x = flow.transform(x)
         return x
+
+
+class DynaFlowRandomVariable(object):
+
+    def __init__(self, dim, time, num_layers, base_dist=None):
+        """Sets up the prelimnary computation graphs."""
+        # Full dimensionality of the latent space.
+        self.full_dim = dim * time
+        if base_dist is None:
+            base_dist = tf.distributions.Normal(
+                loc=np.zeros(self.full_dim), scale=np.ones(self.full_dim))
+        self.dim = dim
+        self.n_time = time
+        self.num_layers = num_layers
+        self.base_dist = base_dist
+        self.flows = []
+        # Set up planar flow layers.
+        self.setup_flow_layers()
+
+    def setup_flow_layers(self):
+        for t in range(self.n_time - 1):
+            self.flows.append([])
+            for i in range(self.num_layers):
+                self.flows[t].append(PlanarFlow(2 * self.dim))
+
+    def sample_log_prob(self, n_samples):
+        """Provide samples from the flow distribution and its log prob."""
+        samples = self.base_dist.sample(n_samples)
+        log_prob = tf.reduce_sum(
+            self.base_dist.log_prob(samples), axis=1)
+        # Transform two consecutive variables in time.
+        final_samples = []
+        single_time_latent_size = [n_samples, self.dim]
+        pre_latent = tf.slice(samples, [0, 0], single_time_latent_size)
+        for i, time_flow in enumerate(self.flows):
+            cur_latent = tf.slice(
+                samples, [0, (i + 1) * self.dim], single_time_latent_size)
+            latent_pair = tf.concat([pre_latent, cur_latent], axis=1)
+            for layer in time_flow:
+                log_prob += layer.log_det_jacobian(latent_pair)
+                latent_pair = layer.transform(latent_pair)
+            # Accumulate the transformed time subsets.
+            final_samples.append(
+                tf.slice(latent_pair, [0, 0], single_time_latent_size))
+            pre_latent = tf.slice(
+                latent_pair, [0, self.dim], single_time_latent_size)
+        # Last time stamp does does not have a following variable.
+        final_samples.append(pre_latent)
+        # Concatenate the subsets to form a single tensor.
+        final_samples = tf.concat(final_samples, axis=1)
+        return final_samples, log_prob
